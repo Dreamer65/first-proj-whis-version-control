7bc756131b1e30d593d746a2666a19e1f5616e15
Sutormin Sergey <sergeysutormen11@live.ru>
2017-10-07 22:28:03 +1100

    v0.0.2

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static NewRoom first;
+    private static NewRoom first;
 
-    public static byte id = 0;
-    public static int quantity = 2;
+    private static byte id = 0;
+    private static int quantity = 2;
 
     public static void main(String[] args) throws IOException {
         Scanner in = new Scanner(System.in);
@@ -80,7 +80,7 @@ public class Main {
                     break;
                 }
 
-                case "connect" : {
+                case "conect" : {
                     System.out.println("С чем соединить?");
                     connect(map, in.nextByte(), in.nextInt(), in.nextInt());
 
@@ -111,7 +111,6 @@ public class Main {
         file.saveLevel(first, id);
 
 
-
         /*
         while (true){
             inputStr = in.next();
@@ -136,138 +135,11 @@ public class Main {
         }
 
 */
-        //////////////////////////////
-/*
-        Room room = new Room("Start", id++);
-        room.printName();
 
-
-        while (true) {
-            String inputStr = in.next();
-
-            switch (inputStr) {
-                case "создать_спереди" : {
-                    if(room.next() != null){
-                        System.out.println("Там уже что-то есть...");
-                        break;
-                    }
-                    String inputStr = in.next();
-                    room.createNext(inputStr, id++);
-                    room = room.next();
-                    room.printName();
-                    break;
-                }
-
-                case "создать_слева" : {
-                    if(room.left() != null){
-                        System.out.println("Там уже что-то есть...");
-                        break;
-                    }
-                    String inputStr = in.next();
-                    room.createLeft(inputStr, id++);
-                    room = room.left();
-                    room.printName();
-                    break;
-                }
-
-                case "создать_справа" : {
-                    if(room.right() != null){
-                        System.out.println("Там уже что-то есть...");
-                        break;
-                    }
-                    String inputStr = in.next();
-                    room.createRight(inputStr, id++);
-                    room = room.right();
-                    room.printName();
-                    break;
-                }
-
-                case "название" : {
-                    room.printName();
-                    break;
-                }
-
-                case "вперед" : {
-                    if(room.next() == null){
-                        System.out.println("Вы пришли в тупик");
-                        break;
-                    }
-                    room = room.next();
-                    room.printName();
-                    break;
-                }
-
-                case "назад" : {
-                    if(room.previous() == null){
-                        System.out.println("Вы пришли в тупик");
-                        break;
-                    }
-                    room = room.previous();
-                    room.printName();
-                    break;
-                }
-
-                case "на_лево" : {
-                    if(room.left() == null){
-                        System.out.println("Вы пришли в тупик");
-                        break;
-                    }
-                    room = room.left();
-                    room.printName();
-                    break;
-                }
-
-                case "на_право" : {
-                    if(room.right() == null){
-                        System.out.println("Вы пришли в тупик");
-                        break;
-                    }
-                    room = room.right();
-                    room.printName();
-                    break;
-                }
-
-                case "вокруг" : {
-                    room.around();
-                    break;
-                }
-
-                case "id" : {
-                    room.getId();
-                    break;
-                }
-
-                case "искать" : {
-                    search(room, in.nextByte());
-                    break;
-                }
-
-                case "выход" : return;
-
-                default : System.out.println("Неизвестная команда");
-            }
-
-
-        }
-
-    }
-
-    public static void search(Room room, byte roomId){
-
-        if(roomId >= id){
-            System.out.println("Такой комнаты нет");
-            return;
-        }
-
-
-
-    }
-*/
 
     }
 
     public static void connect(NewRoom room, byte id, int slot1, int slot2){
-        NewRoom room1 = room;
         NewRoom room2 = room;
 
         if(room2.getId() > id)
@@ -277,16 +149,22 @@ public class Main {
             room2 = room2.getBackDoorNext();
         }
 
-        room1.setTonel(room2, slot1);
-        room2.setTonel(room1, slot2);
+        room.setTonel(room2, slot1);
+        room2.setTonel(room, slot2);
     }
 
     public static NewRoom goTo(NewRoom room, byte id){
         {
-            while (room.getId() != id){
-                room = room.getBackDoorPriveous();
+            if(room.getId() <= id) {
+                while (room.getId() != id) {
+                    room = room.getBackDoorNext();
+                }
             }
-
+            else{
+                while (room.getId() != id) {
+                    room = room.getBackDoorPriveous();
+                }
+            }
             System.out.println("Вы в комнате " + room.getId());
         }
 
